b718ded97361028c5bb0f01022d8daab84ae098f
Joel S. Almeida <joel.almeida.sa@gmail.com>
2022-01-14 22:57:19 -0300

    Cria timer em contagem regressiva. Cria estilos para as bordas das perguntas

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -8,29 +8,54 @@ class Game extends React.Component {
     this.state = {
       options: [],
       count: 0,
+      time: 30,
     };
     this.clickCount = this.clickCount.bind(this);
     this.createButtons = this.createButtons.bind(this);
   }
 
   componentDidMount() {
-    // const { token } = this.props;
     const token = localStorage.getItem('token');
+
     fetchQuestions(token).then((data) => {
       this.setState({ options: data.results });
     });
+
+    this.setTimer();
+  }
+
+  componentDidUpdate() {
+    const { time } = this.state;
+    if (time <= 0) clearInterval(this.intervalId);
+  }
+
+  seconds = (second) => {
+    const milliseconds = 1000;
+    return second * milliseconds;
+  };
+
+  timeCountdown = () => {
+    this.setState((prevState) => ({ time: prevState.time - 1 }));
+  }
+
+  setTimer = () => {
+    this.setState({ time: 30 });
+    clearInterval(this.intervalId);
+    this.intervalId = setInterval(this.timeCountdown, this.seconds(1));
   }
 
   clickCount() {
     this.setState((prevState) => ({
       count: prevState.count + 1,
     }));
+    this.setTimer();
   }
 
   createButtons(options) {
-    const { count } = this.state;
     const { clickCount } = this;
+    const { count } = this.state;
     const { incorrect_answers: incorrect } = options[count];
+
     return incorrect.map((curr, index) => (
       <button
         key={ index }
@@ -44,31 +69,30 @@ class Game extends React.Component {
   }
 
   render() {
-    const { options, count } = this.state;
+    const { options, count, time } = this.state;
     const { clickCount, createButtons } = this;
+
     return (
       <section>
         <div data-testid="question-category">
-          {options.length >= 1
-            ? options[count].category
-            : 'Carregando'}
+          {options.length >= 1 ? options[count].category : 'Carregando'}
         </div>
 
         <div data-testid="question-text">
-          {options.length >= 1
-            ? options[count].question
-            : 'Carregando'}
+          {options.length >= 1 ? options[count].question : 'Carregando'}
         </div>
+
+        <div>{`Tempo: ${time}`}</div>
+
         <div>
           {options.length >= 1 ? createButtons(options) : 'Carregando'}
+
           <button
             type="button"
             onClick={ clickCount }
             data-testid="correct-answer"
           >
-            {options.length >= 1
-              ? options[count].correct_answer
-              : 'Carregando'}
+            {options.length >= 1 ? options[count].correct_answer : 'Carregando'}
           </button>
         </div>
 
