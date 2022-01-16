4cfd7ff69a1298d921be4ad82c97fedd79834079
Vinicius Tanaka <viniciusbtanaka@hotmail.com>
2022-01-16 18:24:15 -0400

    add req #14

--- src/pages/components/Messeges.jsx
+++ src/pages/components/Messeges.jsx
@@ -3,22 +3,26 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 function Messeges(props) {
-  const { assertions } = props;
+  const { assertions, score } = props;
   const minScore = 3;
   const msg = parseInt(assertions, 10) < minScore ? 'Could be better...' : 'Well Done!';
   return (
     <section>
       <h1 data-testid="feedback-text">{msg}</h1>
+      <h2 data-testid="feedback-total-question">{assertions}</h2>
+      <h2 data-testid="feedback-total-score">{score}</h2>
     </section>
   );
 }
 
 Messeges.propTypes = {
   assertions: PropTypes.string.isRequired,
+  score: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-  assertions: state.assertions,
+  assertions: state.player.assertions,
+  score: state.player.score,
 });
 
 export default connect(mapStateToProps)(Messeges);
